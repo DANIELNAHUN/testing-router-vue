c232712fbe5eeb21483a3a533e5060ba240c7e5d
DANIELNAHUN <nahuncf5@gmail.com>
2022-08-08 19:00:18 -0500

    test design layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
+    <div class="router-container">
+      <v-btn><v-icon>mdi-format-list-bulleted-square</v-icon></v-btn>
+      <h2>Testing Vue Router</h2>
+      <div class ="link-container">
       <router-link to="/">Unsplash</router-link> |
       <router-link to="/cardquestion">CardQuestion</router-link> |
       <router-link to="/chronometer">Chronometer</router-link>
-    </v-app-bar>
+      </div>      
+    </div>      
     <slot></slot>
   </div>
 </template>
@@ -19,4 +18,16 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.router-container{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 60px;
+  background: #6a75ab;
+}
+.link-container{
+  display: flex;
+  justify-content:space-evenly;
+  align-items: center;
+}
 </style>
